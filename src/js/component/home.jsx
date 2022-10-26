bf4d55aab1906657fe575e8a4cf325d824720e40
David Suarez <dsuarez001@att.net>
2022-10-26 19:08:57 -0400

    basic ui done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,12 +5,28 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	
-	return (
-		<div className="main-div">
-			
+  return (
+    <div className="container">
+      <div className="main-div">
+        <p>To Do's</p>
+		<div className="input-div">
+			<div className="row">
+				<div className="col-10">
+					<input></input>
+				</div>
+				<div className="col-1">
+					<button>Task</button>
+				</div>
+			</div>
+			<ul className="todo-container">
+				<li>test</li>
+				<li>test</li>
+				<li>test</li>
+			</ul>
 		</div>
-	);
+      </div>
+    </div>
+  );
 };
 
 export default Home;
