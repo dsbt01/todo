def323d52c8962503679cf24dfa37ccefd7c51af
David Suarez <dsuarez001@att.net>
2022-10-28 17:51:18 -0400

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,48 +5,32 @@ let index = -1;
 
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
+  const [, setCount] = useState(0);
 
   function handleRemove(id) {
-    console.log(id);
     let indexToDelete = -1;
 
-	console.log(globalList.length);
-
-    for (let i = 0;i < globalList.length; i++) {
-		console.log("in");
-		console.log(id);
-		console.log(i);
-		if (globalList[i].id === id) {
+    for (let i = 0; i < globalList.length; i++) {
+      if (globalList[i].id === id) {
         indexToDelete = i;
-		console.log("found");
       }
     }
 
-	console.log(indexToDelete);
-
     if (indexToDelete != -1) {
-    	globalList.splice(indexToDelete, 1);
+      globalList.splice(indexToDelete, 1);
 
-      //printTodo();
+      //reset the whole global list
+      for (let i = 0; i < globalList.length; i++) {
+        globalList[i].id = i;
+      }
+
+      index = globalList.length - 1;
+
+      alert("Delete was successful");
+
+      // force a re-render:
+      setCount((c) => c + 1);
     }
-  }
-
-  function printTodo() {
-    const ul = document.getElementById("my-list");
-    let content = "";
-
-    globalList.forEach(function (item) {
-      content =
-        content +
-        "<li key='" +
-        item.id +
-        "'><span>" +
-        item.text +
-        "</span><button>X</button>" +
-        "</li>";
-    });
-
-    ul.innerHTML = content;
   }
 
   function addTodo() {
@@ -55,20 +39,12 @@ const Home = () => {
     } else {
       index += 1;
 
-	  console.log(index);
-
       let newItem = {
         id: index,
         text: inputValue,
       };
 
-	  console.log(newItem);
-
       globalList.push(newItem);
-
-	  console.log(globalList);
-
-      //printTodo();
 
       setInputValue("");
     }
@@ -104,8 +80,12 @@ const Home = () => {
             {globalList.map((item) => (
               <li key={item.id}>
                 <span>{item.text}</span>
-				<span>    </span>
-                <button id="delete-btn" type="button" onClick={() => handleRemove(item.id)}>
+                <span> </span>
+                <button
+                  id="delete-btn"
+                  type="button"
+                  onClick={() => handleRemove(item.id)}
+                >
                   X
                 </button>
               </li>
